22087202cec76672d91a2014a2596af07c08bc0c
vani111222 <vani.loomba23@gmail.com>
2021-04-16 16:26:52 +0530

    MESSAGE WRITTEN BY KARAN

--- sketch.js
+++ sketch.js
@@ -22,7 +22,7 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
-
+platform = new Ground(150,300,300,170);
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
@@ -48,7 +48,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
-
+platform.display();
     box3.display();
     box4.display();
     pig3.display();
